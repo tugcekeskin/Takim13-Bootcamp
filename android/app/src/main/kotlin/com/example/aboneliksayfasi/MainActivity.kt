4ba628866e3c7830c997794a67748abf40cfec2c
Selman Altinbas <selmanaltinbas@gmail.com>
2022-05-06 13:14:45 +0300

    abonelik sayfasına appbar ve bottom bar eklendi

---
+++ android/app/src/main/kotlin/com/example/aboneliksayfasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aboneliksayfasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:aboneliksayfasi/screens/abonelik_sayfasi.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: ThemeData(
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          elevation: 0
+        )
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const AbonelikSayfasi(),
+    );
+  }
+}
